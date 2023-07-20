2c734943fc78b1cc26af65e89bf5c8f386ce97a5
David Whittington <djwhitt@gmail.com>
2023-07-20 14:32:22 -0500

    feat: observer report JSON schema + example draft

--- src/app.ts
+++ src/app.ts
@@ -18,3 +18,95 @@
 import Ajv from "ajv";
 
 const ajv = new Ajv();
+
+const observerReportschema = {
+  type: "object",
+  properties: {
+    reporterAddress: { $ref: "#/$defs/arweaveAddress" },
+    generatedAt: { $ref: "#/$defs/timestamp" },
+    arnsAssessments: { $ref: "#/$defs/arnsAssessments" },
+  },
+  required: ["reporterAddress", "generatedAt", "arnsAssessments"],
+  additionalProperties: false,
+  $defs: {
+    arweaveAddress: {
+      type: "string",
+    },
+    arweaveId: {
+      type: "string",
+    },
+    timestamp: {
+      type: "integer",
+    },
+    arnsName: {
+      type: "string",
+    },
+    hostName: {
+      type: "string",
+    },
+    qosScore: {
+      type: "number",
+      // TODO what range should we use - 0-1? 0-100?
+    },
+    pass: {
+      type: "boolean",
+    },
+    arnsAssessment: {
+      type: "object",
+      properties: {
+        resolvedId: { $ref: "#/$defs/arweaveId" },
+        assessedAt: { $ref: "#/$defs/timestamp" },
+        pass: { $ref: "#/$defs/pass" },
+        qosScore: { $ref: "#/$defs/qosScore" },
+      },
+      additionalProperties: false,
+    },
+    arnsAssessments: {
+      type: "object",
+      patternProperties: {
+        ".*": {
+          type: "object",
+          properties: {
+            prescribedNames: {
+              type: "object",
+              patternProperties: {
+                ".*": { $ref: "#/$defs/arnsAssessment" },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+// NOTE addresses and timestamps are for convience only the chain is the authority
+const exampleReport = {
+  reporterAddress: "xxxx",
+  generatedAt: 1234567890,
+  arnsAssessments: {
+    "example-gateway.net": {
+      prescribedNames: {
+        "example-name-1": {
+          resolvedId: "xxxx",
+          assessedAt: 1234567890,
+          pass: false,
+          qosScore: 0.2,
+        },
+        // 9 more names
+      },
+      chosenNames: {
+        "example-name-2": {
+          resolvedId: "xxxx",
+          assessedAt: 1234567890,
+          pass: true,
+          qosScore: 0.8,
+        },
+        // 39 more names
+      },
+    },
+  },
+};
+
+const valid = ajv.validate(observerReportschema, exampleReport);
+if (!valid) console.log(ajv.errors);
