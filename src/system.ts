88fb85d8e89bd08e7b7a9c69da804130a32aa20c
David Whittington <djwhitt@gmail.com>
2023-10-05 12:45:17 -0500

    refactor: consolidate duplicate server and cli setup into system.ts PE-4712
    This also temporarily removes the ability to specify a host list via the
    CLI. It's not essential at this point (the list comes from the contract
    state), but it will still be restored in a future commit since it's
    useful for testing.

---
+++ src/system.ts
@@ -0,0 +1,85 @@
+/**
+ * AR.IO Observer
+ * Copyright (C) 2023 Permanent Data Solutions, Inc. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import { ChainSource } from './arweave.js';
+import * as config from './config.js';
+import { CachedEntropySource } from './entropy/cached-entropy-source.js';
+import { ChainEntropySource } from './entropy/chain-entropy-source.js';
+import { CompositeEntropySource } from './entropy/composite-entropy-source.js';
+import { RandomEntropySource } from './entropy/random-entropy-source.js';
+import { RemoteCacheHostList } from './hosts/remote-cache-host-list.js';
+import { RandomArnsNamesSource } from './names/random-arns-names-source.js';
+import { RemoteCacheArnsNameList } from './names/remote-cache-arns-name-list.js';
+import { Observer } from './observer.js';
+import { EpochHeightSource } from './protocol.js';
+
+const observedGatewayHostList = new RemoteCacheHostList({
+  baseCacheUrl: 'https://dev.arns.app',
+  contractId: 'bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U',
+});
+
+const chainSource = new ChainSource({
+  arweaveBaseUrl: 'https://arweave.net',
+});
+
+const epochHeightSelector = new EpochHeightSource({
+  heightSource: chainSource,
+});
+
+// TODO remove hard coded values
+const remoteCacheArnsNameList = new RemoteCacheArnsNameList({
+  baseCacheUrl: 'https://dev.arns.app',
+  contractId: 'bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U',
+});
+
+const chainEntropySource = new ChainEntropySource({
+  arweaveBaseUrl: 'https://arweave.net',
+});
+
+const prescribedNamesSource = new RandomArnsNamesSource({
+  nameList: remoteCacheArnsNameList,
+  entropySource: chainEntropySource,
+  numNamesToSource: 1,
+});
+
+const randomEntropySource = new RandomEntropySource();
+
+const cachedEntropySource = new CachedEntropySource({
+  entropySource: randomEntropySource,
+  cachePath: './tmp/entropy',
+});
+
+const compositeEntropySource = new CompositeEntropySource({
+  sources: [cachedEntropySource, chainEntropySource],
+});
+
+const chosenNamesSource = new RandomArnsNamesSource({
+  nameList: remoteCacheArnsNameList,
+  entropySource: compositeEntropySource,
+  numNamesToSource: 1,
+});
+
+export const observer = new Observer({
+  observerAddress: config.OBSERVER_ADDRESS,
+  referenceGatewayHost: config.REFERENCE_GATEWAY_HOST,
+  epochHeightSource: epochHeightSelector,
+  observedGatewayHostList,
+  prescribedNamesSource,
+  chosenNamesSource,
+  gatewayAssessmentConcurrency: config.GATEWAY_ASSESSMENT_CONCURRENCY,
+  nameAssessmentConcurrency: config.NAME_ASSESSMENT_CONCURRENCY,
+});
