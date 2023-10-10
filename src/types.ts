6f261d43048faf6886f6ef592c573dcd9f920179
vilenarios <vilenarios@gmail.com>
2023-10-10 17:30:11 -0400

    feat: adds types for publishing reports

--- src/types.ts
+++ src/types.ts
@@ -64,7 +64,7 @@ export interface ObservationPublisher {
   saveObservations(
     observationReportTxId: string,
     observerReport: ObserverReport,
-  ): Promise<boolean>;
+  ): Promise<string>;
 }
 
 //
