6f5cc01e1c1e81fee5e286e091c248694fb84bb5
David Whittington <djwhitt@gmail.com>
2023-10-23 15:19:52 -0500

    feat: handle missing wallet file PE-4749
    Adds a simple hack to log if the wallet file is missing and skip
    uploading via Turbo if it is.

--- src/turbo.ts
+++ src/turbo.ts
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 import {
+  TurboAuthenticatedClient,
   TurboFactory,
   defaultTurboConfiguration,
 } from '@ardrive/turbo-sdk/node';
@@ -27,11 +28,20 @@ import { KEY_FILE } from './config.js';
 import { ObserverReport } from './types.js';
 
 // load your JWK from a file or generate a new oneW
-const jwk: JWKInterface = JSON.parse(fs.readFileSync(KEY_FILE).toString());
-const turbo = TurboFactory.authenticated({
-  privateKey: jwk,
-  ...defaultTurboConfiguration,
-});
+let jwk: JWKInterface | undefined = undefined;
+try {
+  jwk = JSON.parse(fs.readFileSync(KEY_FILE).toString());
+} catch (error: any) {
+  console.error('Failed to load key file:', error?.message);
+}
+
+let turbo: TurboAuthenticatedClient | undefined;
+if (jwk !== undefined) {
+  turbo = TurboFactory.authenticated({
+    privateKey: jwk,
+    ...defaultTurboConfiguration,
+  });
+}
 
 const tags = [
   { name: 'App-Name', value: 'AR-IO Observer' },
@@ -42,76 +52,86 @@ const tags = [
 export async function uploadReportWithTurbo(
   report: ObserverReport,
 ): Promise<string | null> {
-  let reportTxId = '';
-  // Convert the JSON object to a JSON string
-  const reportString = JSON.stringify(report, null, 2);
-  try {
-    const signer = new ArweaveSigner(jwk);
-    const signedDataItem = createData(reportString, signer, {
-      tags,
-    });
-    await signedDataItem.sign(signer);
-
-    const { id, owner, dataCaches, fastFinalityIndexes } =
-      await turbo.uploadSignedDataItem({
-        dataItemStreamFactory: () => signedDataItem.getRaw(),
-        dataItemSizeFactory: () => signedDataItem.getRaw().length,
+  if (jwk !== undefined && turbo !== undefined) {
+    let reportTxId = '';
+    // Convert the JSON object to a JSON string
+    const reportString = JSON.stringify(report, null, 2);
+    try {
+      const signer = new ArweaveSigner(jwk);
+      const signedDataItem = createData(reportString, signer, {
+        tags,
       });
+      await signedDataItem.sign(signer);
 
-    // upload complete!
-    console.log('Successfully upload report (object) to Turbo!', {
-      id,
-      owner,
-      dataCaches,
-      fastFinalityIndexes,
-    });
-    reportTxId = id;
-  } catch (error) {
-    // upload failed
-    console.error('Failed to upload report (object) to Turbo!', error);
+      const { id, owner, dataCaches, fastFinalityIndexes } =
+        await turbo.uploadSignedDataItem({
+          dataItemStreamFactory: () => signedDataItem.getRaw(),
+          dataItemSizeFactory: () => signedDataItem.getRaw().length,
+        });
+
+      // upload complete!
+      console.log('Successfully upload report (object) to Turbo!', {
+        id,
+        owner,
+        dataCaches,
+        fastFinalityIndexes,
+      });
+      reportTxId = id;
+    } catch (error) {
+      // upload failed
+      console.error('Failed to upload report (object) to Turbo!', error);
+      return null;
+    } finally {
+      const { winc: newBalance } = await turbo.getBalance();
+      console.log('New balance:', newBalance);
+    }
+    return reportTxId;
+  } else {
+    console.error('Key missing, skipping upload');
     return null;
-  } finally {
-    const { winc: newBalance } = await turbo.getBalance();
-    console.log('New balance:', newBalance);
   }
-  return reportTxId;
 }
 
 export async function uploadReportFromDiskWithTurbo(
   fileName: string,
 ): Promise<string | null> {
-  const report = JSON.parse(fs.readFileSync(fileName).toString());
-  let reportTxId = '';
-  // Convert the JSON object to a JSON string
-  const reportString = JSON.stringify(report, null, 2);
-  try {
-    const signer = new ArweaveSigner(jwk);
-    const signedDataItem = createData(reportString, signer, {
-      tags,
-    });
-    await signedDataItem.sign(signer);
-
-    const { id, owner, dataCaches, fastFinalityIndexes } =
-      await turbo.uploadSignedDataItem({
-        dataItemStreamFactory: () => signedDataItem.getRaw(),
-        dataItemSizeFactory: () => signedDataItem.getRaw().length,
+  if (jwk !== undefined && turbo !== undefined) {
+    const report = JSON.parse(fs.readFileSync(fileName).toString());
+    let reportTxId = '';
+    // Convert the JSON object to a JSON string
+    const reportString = JSON.stringify(report, null, 2);
+    try {
+      const signer = new ArweaveSigner(jwk);
+      const signedDataItem = createData(reportString, signer, {
+        tags,
       });
+      await signedDataItem.sign(signer);
 
-    // upload complete!
-    console.log('Successfully uploaded data item from disk to Turbo!', {
-      id,
-      owner,
-      dataCaches,
-      fastFinalityIndexes,
-    });
-    reportTxId = id;
-  } catch (error) {
-    // upload failed
-    console.error('Failed to upload data item from disk to Turbo!', error);
+      const { id, owner, dataCaches, fastFinalityIndexes } =
+        await turbo.uploadSignedDataItem({
+          dataItemStreamFactory: () => signedDataItem.getRaw(),
+          dataItemSizeFactory: () => signedDataItem.getRaw().length,
+        });
+
+      // upload complete!
+      console.log('Successfully uploaded data item from disk to Turbo!', {
+        id,
+        owner,
+        dataCaches,
+        fastFinalityIndexes,
+      });
+      reportTxId = id;
+    } catch (error) {
+      // upload failed
+      console.error('Failed to upload data item from disk to Turbo!', error);
+      return null;
+    } finally {
+      const { winc: newBalance } = await turbo.getBalance();
+      console.log('New balance:', newBalance);
+    }
+    return reportTxId;
+  } else {
+    console.error('Key missing, skipping upload');
     return null;
-  } finally {
-    const { winc: newBalance } = await turbo.getBalance();
-    console.log('New balance:', newBalance);
   }
-  return reportTxId;
 }
