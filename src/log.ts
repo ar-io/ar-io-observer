0c66d77e68dd27f0e732a8603fbf98cb0a7d38b1
David Whittington <djwhitt@gmail.com>
2025-04-01 08:52:21 -0500

    fix(service): ensure tracing is setup before logging PE-7892
    This is needed for Winston instrumentation to work.

--- src/log.ts
+++ src/log.ts
@@ -15,6 +15,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+import './tracing.js';
+
 import { Logger } from '@ar.io/sdk/node';
 import { createLogger, format, transports } from 'winston';
 
